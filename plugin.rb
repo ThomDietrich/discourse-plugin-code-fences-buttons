1c13ce75b98a617dd05bffe5e7ac63743b16d6aa
Alexey Gulev <me@agulev.com>
2021-05-14 20:32:24 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # name: Toolbar Code Fences Buttons (cfbtn)
 # about: Add additional buttons for language-specific code fences to the composer toolbar
 #        https://meta.discourse.org/t/syntax-highlighting-of-code-blocks
-# version: 0.6.0
+# version: 0.7.0
 # authors: Thomas Dietrich
 # url: https://github.com/ThomDietrich/discourse-plugin-code-fences-buttons
 
